baf0e05a961b0e66e17677541e99f776f38ae3e6
Nathan Thomas <nwthomas@me.com>
2019-06-04 00:35:01 -0700

    Setup initial file structure for Sprint server application

---
+++ src/main/java/com/lambdaschool/countryssearch/CountryssearchApplication.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.countryssearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryssearchApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(CountryssearchApplication.class, args);
+	}
+
+}
