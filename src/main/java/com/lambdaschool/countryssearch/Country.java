2b20fe4121a586d4bca106f61592d49a02c365be
Nathan Thomas <nwthomas@me.com>
2019-06-04 09:29:43 -0700

    Build out CountryList.java and countryList ArrayList of countries

--- src/main/java/com/lambdaschool/countryssearch/Country.java
+++ src/main/java/com/lambdaschool/countryssearch/Country.java
@@ -1,5 +1,67 @@
 package com.lambdaschool.countryssearch;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Country
 {
+	private static final AtomicLong counter = new AtomicLong();
+	private long id;
+	private String name;
+	private int population;
+	private int landMass;
+	private int avgAge;
+
+	public Country(String name, int population, int landMass, int avgAge)
+	{
+		this.id = counter.incrementAndGet();
+		this.name = name;
+		this.population = population;
+		this.landMass = landMass;
+		this.avgAge = avgAge;
+	}
+
+	public long getId()
+	{
+		return id;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public int getPopulation()
+	{
+		return population;
+	}
+
+	public void setPopulation(int population)
+	{
+		this.population = population;
+	}
+
+	public int getLandMass()
+	{
+		return landMass;
+	}
+
+	public void setLandMass(int landMass)
+	{
+		this.landMass = landMass;
+	}
+
+	public int getAvgAge()
+	{
+		return avgAge;
+	}
+
+	public void setAvgAge(int avgAge)
+	{
+		this.avgAge = avgAge;
+	}
 }
--- src/main/java/com/lambdaschool/countryssearch/CountryList.java
+++ src/main/java/com/lambdaschool/countryssearch/CountryList.java
@@ -8,6 +8,6 @@ public class CountryList
 
 	public CountryList()
 	{
-		// Finish
+
 	}
 }
