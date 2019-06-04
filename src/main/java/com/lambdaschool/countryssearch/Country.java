9e0aee06d6aac46866436df2440ac75ba1db0818
Nathan Thomas <nwthomas@me.com>
2019-06-04 08:41:58 -0700

    Add Country.java and CountryList.java to app

---
+++ src/main/java/com/lambdaschool/countryssearch/Country.java
@@ -0,0 +1,5 @@
+package com.lambdaschool.countryssearch;
+
+public class Country
+{
+}
---
+++ src/main/java/com/lambdaschool/countryssearch/CountryList.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.countryssearch;
+
+import java.util.ArrayList;
+
+public class CountryList
+{
+	public ArrayList<Country> countryArr = new ArrayList<Country>();
+
+	public CountryList()
+	{
+		// Finish
+	}
+}
